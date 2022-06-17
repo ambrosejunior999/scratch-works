b7c0c8929197c2a1ba134be60ef6bf398773c5d7
Ambrose Junior <adepojuifeoluwa999@gmail.com>
2022-06-17 15:55:03 +0100

    baba

--- card.js
+++ card.js
@@ -1,2 +1,31 @@
 'use strict mode'
+function h61() {
+    setTimeout(() => {
+        
+ 
+    document.getElementById('categories').style.display = 'inherit'
+}, 1000);
+document.getElementById('categories').style.display = 'none'
+}
+function h62() {
+    setTimeout(() => {
+    document.getElementById('electronics').style.display = 'inherit'
+}, 1000);
+
+}
+function h63() {
+    setTimeout(() => {
+    document.getElementById('phones').style.display = 'inherit'
+}, 1000);
+}
+function h64() {
+    setTimeout(() => {
+    document.getElementById('computers').style.display = 'inherit'
+}, 1000);
+}
+function h65() {
+    setTimeout(() => {
+    document.getElementById('clothings').style.display = 'inherit'
+}, 1000);
+}
 
