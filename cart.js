061633c3c077e7046906cdd621addf4fb1fcf3d1
unknown <jeremiahopara4@gmail.com>
2022-06-18 11:31:48 +0100

    2nd commit

--- cart.js
+++ cart.js
@@ -2,4 +2,38 @@
 let visit = document.querySelector('.visit');
 visit.addEventListener('click', function(){
     window.location.href= 'index.html';
-})
+});
+//LinkedIn Quiz
+/* var a = ['dog'];
+a[100] = 'fox';
+console.log(a.length);let rainForests = ['Amaz','b','c','d'];
+rainForests.splice(0,2);
+console.log(rainForests)
+3!='3';
+
+const numbers = [1,2];
+const[one,two,three]= numbers;
+console.log(one);
+let vowels = Array.of('aeiou');
+for(var i =0;i<vowels.length;i++){
+    console.log(vowels[i]);
+}
+const x = 6%2;
+const y = x ? 'One': 'Two';
+console.log(y);
+var cat = {name:"Athena"};
+function swap(feline){
+    feline.name = "wild";
+    feline = {name:"Tabby"};
+}
+swap(cat);
+console.log(cat.name);
+
+const myFunc = () =>{
+    const a =2;
+    return () => console.log("a is " + a);
+};
+const a = 1;
+const test = myFunc();
+test();
+ */
